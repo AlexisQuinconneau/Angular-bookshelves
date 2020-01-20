e8df7d952706051d0f56acb0a23a5f84aaea46dc
alexis quinconneau <alexis.quinconneau@lapiscine.pro>
2020-01-20 11:37:17 +0100

    Application terminée

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import * as firebase from 'firebase';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,17 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'bookshelves';
+  constructor() {
+    var firebaseConfig = {
+      apiKey: "AIzaSyDGJnXMGFbKCmSs497ckCjj1bLfQVl--Ls",
+      authDomain: "bookshelves-55025.firebaseapp.com",
+      databaseURL: "https://bookshelves-55025.firebaseio.com",
+      projectId: "bookshelves-55025",
+      storageBucket: "bookshelves-55025.appspot.com",
+      messagingSenderId: "875440026535",
+      appId: "1:875440026535:web:5e04a9a9eb30f9b3ba3c82"
+    };
+    // Initialize Firebase
+    firebase.initializeApp(firebaseConfig);
+  }
 }
